61916d0635c073dd5e51d3b9904af74b1955af54
mohmmedElAmrani <elamranimohammed104@gmail.com>
2023-05-03 02:04:50 +0100

    this is a main change

--- hello.js
+++
@@ -1,9 +0,0 @@
-
-
-console.log("Hi it is me")
-console.log("Hi it is me")
-console.log("Hi it is me")
-console.log("Hi it is me")
-console.log("Hi it is me")
-console.log("Hi it is me")
-console.log("Hi it is me")
